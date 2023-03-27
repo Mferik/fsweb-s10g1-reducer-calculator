5dc81313d024fd331f8278ee8f3c9d70e104f613
Mferik <mfatiherikk@gmail.com>
2023-03-27 17:35:15 +0300

    S10-G1 Tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useReducer } from "react";
 
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
+import reducer, { initialState } from "./reducers";
+import {
+  ADD_ONE,
+  APPLY_NUMBER,
+  CHANGE_OPERATION,
+  CLEAR_DISPLAY,
+  MEMORY_CLEAR,
+  MEMORY_PLUS,
+  MEMORY_RECALL,
+} from "./actions";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -13,47 +25,102 @@ function App() {
       <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation">
+                <b>Operation:</b> {state.operation}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {state.memory}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={"M+"} />
-              <CalcButton value={"MR"} />
-              <CalcButton value={"MC"} />
+              <CalcButton
+                value={"M+"}
+                onClick={() => dispatch({ type: MEMORY_PLUS, payload: "M+" })}
+              />
+              <CalcButton
+                value={"MR"}
+                onClick={() => dispatch({ type: MEMORY_RECALL, payload: "MR" })}
+              />
+              <CalcButton
+                value={"MC"}
+                onClick={() => dispatch({ type: MEMORY_CLEAR, payload: "MC" })}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+            <CalcButton value={1} onClick={() => dispatch({ type: ADD_ONE, payload: 1 })} />
+
+
+              <CalcButton
+                value={2}
+                onClick={() => dispatch({ type: APPLY_NUMBER, payload: 2 })}
+              />
+              <CalcButton
+                value={3}
+                onClick={() => dispatch({ type: APPLY_NUMBER, payload: 3 })}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton
+                value={4}
+                onClick={() => dispatch({ type: APPLY_NUMBER, payload: 5 })}
+              />
+              <CalcButton
+                value={5}
+                onClick={() => dispatch({ type: APPLY_NUMBER, payload: 6 })}
+              />
+              <CalcButton
+                value={6}
+                onClick={() => dispatch({ type: APPLY_NUMBER, payload: 7 })}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton
+                value={7}
+                onClick={() => dispatch({ type: APPLY_NUMBER, payload: 7 })}
+              />
+              <CalcButton
+                value={8}
+                onClick={() => dispatch({ type: APPLY_NUMBER, payload: 8 })}
+              />
+              <CalcButton
+                value={9}
+                onClick={() => dispatch({ type: APPLY_NUMBER, payload: 9 })}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton
+                value={"+"}
+                onClick={() =>
+                  dispatch({ type: CHANGE_OPERATION, payload: "+" })
+                }
+              />
+              <CalcButton
+                value={"*"}
+                onClick={() =>
+                  dispatch({ type: CHANGE_OPERATION, payload: "*" })
+                }
+              />
+              <CalcButton
+                value={"-"}
+                onClick={() =>
+                  dispatch({ type: CHANGE_OPERATION, payload: "-" })
+                }
+              />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton
+                value={"CE"}
+                onClick={() => dispatch({ type: CLEAR_DISPLAY, payload: "CE" })}
+              />
             </div>
-
           </form>
         </div>
       </div>
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
 export const ADD_ONE = "ADD_ONE";
-
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_PLUS = "MEMORY_PLUS";
+export const MEMORY_RECALL = "MEMORY_RECALL";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
+export const ADD_DIGIT = "ADD_DIGIT";
 
 export const addOne = () => {
   return ({ type: ADD_ONE });
@@ -9,4 +13,30 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
   return ({ type: APPLY_NUMBER, payload: number });
+}
+
+export function addDigit(digit) {
+  return {
+    type: ADD_DIGIT,
+    payload: digit,
+  };
+}
+export const changeOperation = (operator) => {
+  return {type: CHANGE_OPERATION, payload:operator}
+}
+
+export const clearDisplay = () => {
+  return {type:CLEAR_DISPLAY}
+}
+
+export const memoryPlus = () => {
+  return {type: MEMORY_PLUS}
+}
+
+export const memoryClear = () => {
+  return {type: MEMORY_CLEAR}
+}
+
+export const memoryRecall = () => {
+  return {type: MEMORY_RECALL}
 }
